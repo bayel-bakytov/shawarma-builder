1d09b209c66facadf66b2dd6819ad8851a653c5a
Bayel <bayel.bakytovich@gmail.com>
2020-03-29 21:38:53 +0600

    Add basic styling

--- src/components/Layout/Drawer/Drawer.js
+++ src/components/Layout/Drawer/Drawer.js
@@ -1,7 +1,7 @@
 import React from "react";
-
+import classes from "./Drawer.module.css";
 export default () => (
-    <div className="Drawer">
+    <div className={classes.Drawer}>
        Drawer
     </div>
 );
--- src/components/Layout/Toolbar/Toolbar.js
+++ src/components/Layout/Toolbar/Toolbar.js
@@ -1,7 +1,8 @@
 import React from "react";
+import classes from"./Toolbar.module.css";
 
 export default () => (
-    <div className="Toolbar">
-      Toolbar
+    <div className={classes.Toolbar}>
+     Toolbar
     </div>
 );
