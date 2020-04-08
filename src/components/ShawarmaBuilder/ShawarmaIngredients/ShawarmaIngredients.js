e9963f43d77b1d0e635902d5822047617187eb5a
Bayel <bayel.bakytovich@gmail.com>
2020-04-09 01:02:12 +0600

    Use state in ShawarmaIngredients,ShawarmaBuilder components

--- src/components/ShawarmaBuilder/ShawarmaIngredients/ShawarmaIngredients.js
+++ src/components/ShawarmaBuilder/ShawarmaIngredients/ShawarmaIngredients.js
@@ -4,10 +4,20 @@ import classes from "./ShawarmaIngredients.module.css";
 
 
 
-export default () => (
+export default  ({ ingredients }) => {
+  let ingredientsOutput = [];
+   
+
+  Object.keys(ingredients).forEach(type => {
+    for (let i = 0; i <ingredients[type]; i++) {
+      ingredientsOutput.push(<Ingredients key={type + i} type={type}/>);
+    }
+  });
+
+
+  return(
         <div className={classes.ShawarmaIngredients}>
-          <Ingredients type="tomato"/>
-          <Ingredients type="cucumber"/>
-          <Ingredients type="french-fries"/>
+         {ingredientsOutput}
         </div>  
-);
+  );
+};
--- src/containers/ShawarmaBuilder/ShawarmaBuilder.js
+++ src/containers/ShawarmaBuilder/ShawarmaBuilder.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from "react";
 import ShawarmaIngredients from "../../components/ShawarmaBuilder/ShawarmaIngredients/ShawarmaIngredients";
 import classes from "./ShawarmaBuilder.module.css";
 
-
-export default () => (
-        <div className={classes.ShawarmaBuilder}>
-           <ShawarmaIngredients />
-           IngredientsControl
-        </div>  
-);
+export default () => {
+  const [ingredients, setIngredients] = useState({
+          cucumber:5,
+          tomato:5,
+          frenchFries:6,
+  });
+  return (
+    <div className={classes.ShawarmaBuilder}>
+      <ShawarmaIngredients ingredients={ingredients}/>
+      IngredientsControl
+    </div>
+  );
+};
