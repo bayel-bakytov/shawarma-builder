19dd5bfb8485019937cc2d4b80895725e47cb2f0
Bayel <bayel.bakytovich@gmail.com>
2020-04-12 21:19:53 +0600

    Add random

--- src/components/ShawarmaBuilder/ShawarmaIngredients/Ingredients/Ingredients.js
+++ src/components/ShawarmaBuilder/ShawarmaIngredients/Ingredients/Ingredients.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React from "react";
 import classes from "./Ingredients.module.css";
 
-
 export default ({ type }) => {
-    const IngredientsClasses = [classes.Ingredients];
+  const IngredientsClasses = [classes.Ingredients];
 
-    switch (type) {
-        case "tomato":
-            IngredientsClasses.push(classes.tomato);
-            break;
-        case "cucumber":
-            IngredientsClasses.push(classes.cucumber);
-            break;
-        case "french-fries":
-        default:
-            IngredientsClasses.push(classes.frenchFries);
-            break;
-    }
-    return (
-        <div className={IngredientsClasses.join(" ")}>
+  let stylePos = null;
+  const getPosition = (ir) => {
+    const pd = 540;
+    const pr = pd / 2;
 
-        </div>
-    )
+    const ix = Math.round(Math.random() * pd);
+    const iy = Math.round(Math.random() * pd);
 
-}
+    const distance =
+      Math.sqrt(Math.pow(ix - pr, 2) + Math.pow(iy - pr, 2)) + ir;
+
+    return distance < pr ? { x: ix - ir, y: iy - ir } : getPosition(ir);
+  };
+
+  switch (type) {
+    case "tomato":
+      IngredientsClasses.push(classes.tomato);
+      break;
+    case "cucumber":
+      IngredientsClasses.push(classes.cucumber);
+      break;
+    case "french-fries":
+    default:
+      IngredientsClasses.push(classes.frenchFries);
+      break;
+  }
+
+  const position = getPosition(35 / 2);
+
+  stylePos = {
+    position: "absolute",
+    top: position.y + "px",
+    left: position.x + "px",
+  };
+
+  return <div style={stylePos} className={IngredientsClasses.join(" ")}></div>;
+};
