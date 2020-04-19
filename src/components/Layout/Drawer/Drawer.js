7d5b123935d5ddc965d52a299e57565666cae33d
Bayel <bayel.bakytovich@gmail.com>
2020-04-19 15:45:18 +0600

    Added to Drawer:Logo and Navigation Component

--- src/components/Layout/Drawer/Drawer.js
+++ src/components/Layout/Drawer/Drawer.js
@@ -1,7 +1,10 @@
 import React from "react";
 import classes from "./Drawer.module.css";
+import Logo from "../../UI/Logo/Logo";
+import Navigation from "../../UI/Navigation/Navigation";
 export default () => (
     <div className={classes.Drawer}>
-    
+     <Logo/>
+     <Navigation/>
     </div>
 );
