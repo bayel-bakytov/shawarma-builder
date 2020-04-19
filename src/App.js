ba334a3a67b10b4814b6d39bef28724b10a97807
Bayel <bayel.bakytovich@gmail.com>
2020-04-19 22:27:12 +0600

    Remove Layout component to containers folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Layout from './components/Layout/Layout';
+import Layout from './containers/Layout/Layout';
 import ShawarmaBuilder from './containers/ShawarmaBuilder/ShawarmaBuilder';
 
 export default  () => {
--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import Drawer from "./Drawer/Drawer";
-import Toolbar from "./Toolbar/Toolbar";
+import Drawer from "../../components/Layout/Drawer/Drawer";
+import Toolbar from "../../components/Layout/Toolbar/Toolbar";
 import classes from "./Layout.module.css";
 
 export default ({ children }) => {
