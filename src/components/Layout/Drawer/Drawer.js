0dd2267aff8864eecee9a8c1ac745649d8dabe93
Bayel <bayel.bakytovich@gmail.com>
2020-04-19 22:21:27 +0600

    Create drawer state

--- src/components/Layout/Drawer/Drawer.js
+++ src/components/Layout/Drawer/Drawer.js
@@ -3,12 +3,17 @@ import classes from "./Drawer.module.css";
 import Logo from "../../UI/Logo/Logo";
 import Navigation from "../../UI/Navigation/Navigation";
 import Backdrop from "../../UI/Backdrop/Backdrop";
-export default () => (
-  <div className={classes.Drawer}>
-    <Backdrop show/>
-    <section className={classes.content}>
-      <Logo />
-      <Navigation />
-    </section>
-  </div>
-);
+export default ({ switchDrawer, open }) => {
+  const contentClasses = [classes.content];
+  contentClasses.push(open ? classes.Open : classes.Close);
+
+  return (
+    <div className={classes.Drawer}>
+      <Backdrop hideCallback={switchDrawer} show={open} />
+      <div className={contentClasses.join(" ")}>
+        <Logo />
+        <Navigation />
+      </div>
+    </div>
+  );
+};
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Drawer from "./Drawer/Drawer";
 import Toolbar from "./Toolbar/Toolbar";
 import classes from "./Layout.module.css";
 
-export default ({ children }) => (
-  <div className={classes.Layout}>
-    <Toolbar />
-    <Drawer />
-    <main>
-       {children}
-    </main>
-  </div>
-);
+export default ({ children }) => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  return (
+    <div className={classes.Layout}>
+      <Toolbar />
+      <Drawer open={drawerOpen} switchDrawer={() => {}} />
+      <main>{children}</main>
+    </div>
+  );
+};
