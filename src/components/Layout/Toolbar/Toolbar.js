e9856f8e3ad229b9fbdd9fc78361db543e3796bc
Bayel <bayel.bakytovich@gmail.com>
2020-04-19 15:41:51 +0600

    Move to UI folder

--- src/components/Layout/Toolbar/Toolbar.js
+++ src/components/Layout/Toolbar/Toolbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import classes from "./Toolbar.module.css";
-import Logo from "../Logo/Logo";
-import Navigation from "./Navigation/Navigation";
+import Logo from "../../UI/Logo/Logo";
+import Navigation from "../../UI/Navigation/Navigation";
 
 export default () => (
   <div className={classes.Toolbar}>
