b41b37091bd510bc6be866a19760d760d9c3806e
Bayel <bayel.bakytovich@gmail.com>
2020-05-09 18:09:15 +0600

    Added Route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Layout from "./containers/Layout/Layout";
 import ShawarmaBuilder from "./containers/ShawarmaBuilder/ShawarmaBuilder";
 import Checkout from "./containers/Checkout/Checkout";
@@ -8,8 +9,17 @@ export default () => {
   return (
     <div className="App">
       <Layout>
-        <ShawarmaBuilder />
-        <Checkout />
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/builder" />
+          </Route>
+          <Route path="/builder">
+            <ShawarmaBuilder />
+          </Route>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+        </Switch>
       </Layout>
     </div>
   );
