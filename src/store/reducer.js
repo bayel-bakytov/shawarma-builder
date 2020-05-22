7c366f5a7b1358883a9682b10ee6ae629771febe
Bayel <bayel.bakytovich@gmail.com>
2020-05-22 18:02:43 +0600

    Add action types

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,20 @@
+import * as actions from "./actions";
+
 const intialState = {
   ingredients: null,
   price: 100,
 };
 
 export default (state = intialState, action) => {
+  switch (action.type) {
+    case actions.ADD_INGREDIENT:
+      return state;
+
+    case actions.REMOVE_INGREDIENT:
+      return state;
+
+    default:
+      return state;
+  }
   return state;
 };
