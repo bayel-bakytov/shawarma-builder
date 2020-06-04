8bd30ec25c3962c7b364a3cd493ac132b5ae7f50
Bayel <bayel.bakytovich@gmail.com>
2020-06-04 23:53:40 +0600

    Use redux actions in auth component

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,15 +1,28 @@
 import React from "react";
 import axios from "axios";
+import { start, auth } from "../../store/actions/auth";
 import withAxios from "../../hoc/withAxios/withAxios";
 import classes from "./Auth.module.css";
 import Button from "../UI/Button/Button";
+import { useDispatch } from "react-redux";
 
 export default withAxios(() => {
+  const dispatch = useDispatch();
+
+  const formSubmitted = (event) => {
+    start(dispatch);
+
+    const data = new FormData(event.target);
+    auth(dispatch, data.get("email"), data.get("password"));
+
+    event.preventDefault();
+  };
+
   return (
     <div className={classes.Auth}>
-      <form>
+      <form onSubmit={formSubmitted}>
         <h1>Sign up</h1>
-        <input type="text" placeholder="E-mail" name="email" required />
+        <input type="email" placeholder="E-mail" name="email" required />
         <input
           type="password"
           placeholder="Password"
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -3,5 +3,4 @@ import builder from "./builder";
 import orders from "./orders";
 import auth from "./auth";
 
-export default combineReducers({ builder, orders });
 export default combineReducers({ builder, orders, auth });
